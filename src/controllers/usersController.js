058efb28ef7b049b12ea3eaf5ac9e37861785e7d
MarceloRPC <marcelo.paiva@argobr.com>
2025-02-14 11:32:02 -0300

    implements async errors treatment

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,12 @@
+import AppError from "../utils/appError.js";
+
 class UsersController {
   create(request, response) {
     const { name } = request.body;
+
+    if (!name) {
+      throw new AppError("The 'name' is required!");
+    }
 
     response.status(201).json({
       name
--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
-import express from "express";
+import express, { response } from "express";
 import routes from "./routes/index.js";
+import AppError from "./utils/appError.js";
+import "express-async-errors";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 
 const PORT = 3333;
 app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
